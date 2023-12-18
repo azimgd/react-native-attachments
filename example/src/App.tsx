976a95ae746f9e139a2058c6693f65b9513d62e8
azim <me@azimgd.com>
2023-12-18 15:55:39 +0200

    feat: image and file picker integration

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 import {
   useCompleteFlow,
+  pickImage,
+  pickFile,
   type IProgressCallback,
   type IAttachmentItem,
+  type IPickDocumentSuccessCallback,
+  type IPickImageSuccessCallback,
 } from 'react-native-attachments';
 
 const DEFAULT_ATTACHMENTS_STATE: Partial<IAttachmentItem>[] = [
@@ -79,15 +83,48 @@ export default function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  React.useEffect(() => {
-    setTimeout(() => completeFlow.completeFlow(), 1000);
+  const handlePickImage = React.useCallback(() => {
+    const onSuccess: IPickImageSuccessCallback = (assets) => {
+      const modifiedAssets = assets.map((asset) => ({
+        path: asset.uri!,
+        type: 'IMAGE' as 'IMAGE',
+      }));
+      completeFlow.addAttachments(modifiedAssets);
+    };
+
+    pickImage({ mediaType: 'mixed' }, { onSuccess });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handlePickFile = React.useCallback(() => {
+    const onSuccess: IPickDocumentSuccessCallback = (assets) => {
+      const modifiedAssets = assets.map((asset) => ({
+        path: asset.uri!,
+        type: 'FILE' as 'FILE',
+      }));
+      completeFlow.addAttachments(modifiedAssets);
+    };
+
+    pickFile({}, { onSuccess });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <View style={styles.container}>
+      <Pressable style={styles.button} onPress={handlePickImage}>
+        <Text>Pick image</Text>
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={handlePickFile}>
+        <Text>Pick file</Text>
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={completeFlow.completeFlow}>
+        <Text>Process all</Text>
+      </Pressable>
+
       {completeFlow.attachments.map((attachment, index) => (
-        <Text key={index}>
+        <Text style={styles.text} key={index}>
           {attachment.path} {attachment.action} {attachment.status}
         </Text>
       ))}
@@ -98,12 +135,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    padding: 32,
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  button: {
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: '#ececec',
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+    borderRadius: 16,
+    marginBottom: 32,
+  },
+  text: {
+    marginBottom: 10,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,2 @@
 export * from './attachments';
+export * from './picker';
